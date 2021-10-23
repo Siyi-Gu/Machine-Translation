efb5ce1a69996694f8aa4d504d1cc5a502c79205
siyigu <nullviv41@gmail.com>
2021-10-23 21:17:53 +0200

    update gu_train

--- gu_preprocess.sh
+++ gu_preprocess.sh
@@ -1,22 +1,23 @@
 DATA=/mnt/Machine-Translation/processed/gu
 TRAIN=train
 VALID=valid
-TEST=test
+# TEST=test
 SRC=gu_IN
 TGT=en_XX
 NAME=gu-en
-DEST=/mnt/Machine-Translation/processed/gu
+DEST=/mnt/Machine-Translation/processed/new_gu
 DICT=/mnt/mbart.cc25.v2/dict.txt
 
 fairseq-preprocess \
---source-lang ${SRC} \
---target-lang ${TGT} \
---trainpref ${DATA}/${TRAIN}.spm \
---validpref ${DATA}/${VALID}.spm \
---testpref ${DATA}/${TEST}.spm  \
---destdir ${DEST}/${NAME} \
---thresholdtgt 0 \
---thresholdsrc 0 \
---srcdict ${DICT} \
---tgtdict ${DICT} \
---workers 70
+    --source-lang ${SRC} \
+    --target-lang ${TGT} \
+    --trainpref ${DATA}/${TRAIN}.spm \
+    --validpref ${DATA}/${VALID}.spm \
+    --destdir ${DEST}/${NAME} \
+    --thresholdtgt 0 \
+    --thresholdsrc 0 \
+    --srcdict ${DICT} \
+    --tgtdict ${DICT} \
+    --workers 70
+
+# --testpref ${DATA}/${TEST}.spm  \
