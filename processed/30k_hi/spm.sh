37b972623d087fda238b9472c034697c2e4ce571
bocabolala <geekkoala7@gmail.com>
2021-10-27 01:35:27 +0200

    30k hindi sentences after spm

---
+++ processed/30k_hi/spm.sh
@@ -0,0 +1,11 @@
+SPM=/Users/sig/sentencepiece/build/src/spm_encode
+MODEL=/Users/sig/Documents/Sy/mbart.cc25.v2/sentence.bpe.model
+DATA=/Users/sig/Documents/Sy/Machine-Translation/processed/30k_hi
+TRAIN=train
+VALID=valid
+SRC=hi_IN
+TGT=en_XX
+${SPM} --model=${MODEL} < ${DATA}/${TRAIN}.${SRC} > ${DATA}/${TRAIN}.spm.${SRC} &
+${SPM} --model=${MODEL} < ${DATA}/${TRAIN}.${TGT} > ${DATA}/${TRAIN}.spm.${TGT} &
+${SPM} --model=${MODEL} < ${DATA}/${VALID}.${SRC} > ${DATA}/${VALID}.spm.${SRC} &
+${SPM} --model=${MODEL} < ${DATA}/${VALID}.${TGT} > ${DATA}/${VALID}.spm.${TGT} &
